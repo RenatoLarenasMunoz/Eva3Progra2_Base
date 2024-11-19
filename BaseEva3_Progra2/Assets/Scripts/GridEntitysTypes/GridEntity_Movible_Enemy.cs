ee7031cf5c7ac8f87ab1272fda1d6c99a9597a32
RenatoLarenasMuñoz <renato.larenas@zonavirtual.uisek.cl>
2024-11-19 16:47:08 -0300

    avance
    enemigo tiene direccion

--- BaseEva3_Progra2/Assets/Scripts/GridEntitysTypes/GridEntity_Movible_Enemy.cs
+++ BaseEva3_Progra2/Assets/Scripts/GridEntitysTypes/GridEntity_Movible_Enemy.cs
@@ -6,10 +6,11 @@ public class GridEntity_Movible_Enemy : GridEntity_Movible
 {
     public GridEntity_Movible_Player player;
     public GridShooter gridShooter;
+    public EnemyType enemyType;
     public float damage;
+    public bool isStun;
     public Vector2Int startPos;
     public Vector2Int playerOldPos;
-    public Vector2Int playerPos;
 
 
     protected override void Awake2()
@@ -31,16 +32,45 @@ public class GridEntity_Movible_Enemy : GridEntity_Movible
 
     protected override void Update2()
     {
-        playerPos = player.gridPos;
-
-        if (playerPos != playerOldPos)
+        /*switch (enemyType)
         {
-            //isMoving = true;
-            playerOldPos = playerPos;
+            case EnemyType.Basic:
+                break;
+            case EnemyType.Tank:
+                break;
+        }*/
 
-            //Vector2Int dir = Vector2Int.zero;
+        if (player.gridPos != playerOldPos)
+        {
+            Vector2Int dir = player.gridPos - gridPos;
+            if (dir.x > 0)
+            {
+                dir.x = 1;
+            }
+            else if (dir.x < 0)
+            {
+                dir.x = -1;
+            }
+            else if (dir.y > 0)
+            {
+                dir.y = 1;
+            }
+            else if (dir.y < 0)
+            {
+                dir.y = -1;
+            }
 
-            //Move(dir);
+            if (!isStun)
+            {
+                isMoving = true;
+                Move(dir);
+            }
+            else
+            {
+                isStun = false;
+            }
+
+            playerOldPos = player.gridPos;
         }
 
         if (gridPos == player.gridPos)
--- BaseEva3_Progra2/Assets/Scripts/MyEnums.cs
+++ BaseEva3_Progra2/Assets/Scripts/MyEnums.cs
@@ -20,5 +20,4 @@ public enum EnemyType
 {
     Basic,
     Tank,
-    Fast,
 }
